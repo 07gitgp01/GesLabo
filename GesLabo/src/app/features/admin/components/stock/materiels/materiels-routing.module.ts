95402fca191d3820db2ccab21e690c802ef15a38
W.Paulin GUIGMA <guigmawpaulin@gmail.com>
2025-02-23 19:09:23 +0000

    new recup

--- GesLabo/src/app/features/admin/components/stock/materiels/materiels-routing.module.ts
+++ GesLabo/src/app/features/admin/components/stock/materiels/materiels-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MaterielsListeComponent } from './materiels-liste/materiels-liste.component';
 
+
 const routes: Routes = [
-  { path: 'materiels', component: MaterielsListeComponent}
+  { path: 'materiels', component: MaterielsListeComponent},
+  // { path: 'materiels/ajout', component: MaterielAjoutComponent},
+  { path: 'materiels/modif', component: MaterielsListeComponent}, 
+  { path: 'materiels/details', component: MaterielsListeComponent},
 ];
 
 @NgModule({
